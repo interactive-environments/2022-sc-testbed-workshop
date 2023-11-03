704b2b45c7b86059fd85bc7619fca851fe61998a
Robin Rietdijk <robinrietdijk@kabelfoon.net>
2023-11-03 11:28:08 +0100

    Fix faulty imports in PBS_mqtt

--- ItsyBitsy_Node/lib/PBS_mqtt.py
+++ ItsyBitsy_Node/lib/PBS_mqtt.py
@@ -1,6 +1,6 @@
 # Write your code here :-)
-import adafruit_esp32spi.adafruit_esp32spi_socket as socket
-import adafruit_minimqtt as miniMQTT
+from adafruit_esp32spi import adafruit_esp32spi_socket as socket
+from adafruit_minimqtt import adafruit_minimqtt as miniMQTT
 class PBSMQTT():
     def __init__(self, esp, settings, noise):
         self.settings = settings
